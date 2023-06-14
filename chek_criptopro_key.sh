bb311d55479e0472dc938fbae30ddf89521b256f
Sergey Starkevich <sergey.starkevich@gmail.com>
2023-06-14 11:12:12 +0300

    crypto

--- chek_criptopro_key.sh
+++ chek_criptopro_key.sh
@@ -5,7 +5,7 @@ if [ -d "$DIR" ]; then
     HOSTNAME=$(hostname)
     mkdir /Library/Audio/share  
     mount_smbfs //"CORPSC;tech-ksmg:XRlRv2lc8t4SslHc41Q"@dc01-fs-01.corp.sbercloud.ru/Reports/CryptoPro-mac/ /Library/Audio/share
-   # mount_smbfs //skstarkeevich:Reanimator_02@dc01-fs-01.corp.sbercloud.ru/Reports/CryptoPro-mac/ /Library/Audio/share
+   # mount_smbfs //user:pass@dc01-fs-01.corp.sbercloud.ru/Reports/CryptoPro-mac/ /Library/Audio/share
     echo $HOSTNAME  $SN >> /Library/Audio/share/test.txt
     umount /Library/Audio/share
     rm -r /Library/Audio/share
