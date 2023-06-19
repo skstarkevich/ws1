7c74f53d1d76ef2bb2b38bdccd65a1c053148679
Sergey Starkevich <sergey.starkevich@gmail.com>
2023-06-19 13:01:11 +0300

    dell

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-NEW_VER = 14
-VER = $( defaults read /Library/Application\ Support/Kaspersky\ Lab/KAV/Applications/Kaspersky\ Anti-Virus\ Agent.app/Contents/Info.plist CFBundleShortVersionString )
-if [ "$NEW_VER" == "$VER" ]; then
-    echo 0
-    elso
-    echo 1
-fi
