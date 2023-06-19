7b21855e536d0af6faa4895a183e7e547d7d6a37
Sergey Starkevich <sergey.starkevich@gmail.com>
2023-06-19 12:59:36 +0300

    macos 2

--- macos/check_cryptopro_key.sh
+++ macos/check_cryptopro_key.sh
@@ -10,8 +10,7 @@ if [ -d "$DIR" ]; then
     HOSTNAME=$(hostname)
     DATE=$(date)
     mkdir /Library/Audio/share  
-    mount_smbfs //"CORPSC;tech-ksmg:XRlRv2lc8t4SslHc41Q"@dc01-fs-01.corp.sbercloud.ru/Reports/CryptoPro-mac/ /Library/Audio/share
-   # mount_smbfs //user:pass@dc01-fs-01.corp.sbercloud.ru/Reports/CryptoPro-mac/ /Library/Audio/share
+    mount_smbfs //user:pass@dc01-fs-01.corp.sbercloud.ru/Reports/CryptoPro-mac/ /Library/Audio/share
     echo $HOSTNAME  $SN $DATE >> /Library/Audio/share/test.txt
     umount /Library/Audio/share
     rm -r /Library/Audio/share
