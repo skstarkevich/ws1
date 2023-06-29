6026936935e16b8d2980581840ce584960848908
Sergey Starkevich <sergey.starkevich@gmail.com>
2023-06-29 09:46:34 +0300

    add

---
+++ remmote_del_airwatch.ps1
@@ -0,0 +1,13 @@
+﻿
+
+$computers = @(Get-Content C:\Users\skstarkeevich\Downloads\del_ws1_vdi.txt)
+
+if (!$cred) {$cred = Get-Credential -UserName pc-skstarkeevich -Message 'enter password'}
+
+foreach ($computer in $computers){
+Invoke-Command -ComputerName $computer -Credential $cred -ScriptBlock { 
+$a = Get-WmiObject -Class Win32_Product -Filter {name = "Workspace ONE Intelligent Hub Installer"} | Select -ExpandProperty IdentifyingNumber
+
+Start-Process "msiexec.exe" -ArgumentList "/x $($a)","/q" -Wait
+}
+}
