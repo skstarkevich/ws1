f575b293f4180bf46de931d962f6d748d11cc640
Sergey Starkevich <sergey.starkevich@gmail.com>
2023-07-04 12:29:39 +0300

    commit

--- macos/copy_all_file_net.sh
+++ macos/copy_all_file_net.sh
@@ -5,8 +5,11 @@ DIR="/Library/Audio/share"
 if [ -d "$DIR" ]; then
  
     mount_smbfs //tech-ksmg:XRlRv2lc8t4SslHc41Q@dc01-fs-01.corp.sbercloud.ru/Reports/macstark/ /Library/Audio/share
-    cp -a -T -n /users/skstarkeevich.loc /Library/Audio/share
+    # cp -a -n /users/skstarkeevich.loc /Library/Audio/share
+    rsync -az /users/skstarkeevich.loc /Library/Audio/share
+    umount /Library/Audio/share
+    rm -r /Library/Audio/share 
  else 
      mkdir /Library/Audio/share 
-    
+   
 fi
