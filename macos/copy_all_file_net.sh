9c96f1c70a4c949dfbceac0e18a8968c4dd9cdc6
Sergey Starkevich <sergey.starkevich@gmail.com>
2023-07-19 12:35:47 +0300

    copy

--- macos/copy_all_file_net.sh
+++ macos/copy_all_file_net.sh
@@ -6,8 +6,8 @@ if [ -d "$DIR" ]; then
  
     mount_smbfs //login:pass@dc01-fs-01.corp.sbercloud.ru/Reports/macstark/ /Library/Audio/share
     #timeout 3540 cp -a -n /users/test /Library/Audio/share
-    rsync -azruq ={'Архив Outlook для Mac 29.06.2022.olm','/Library/Audio/share'} /users/test /Library/Audio/share
-    
+    rsync -azruq  --exclude={'Архив Outlook для Mac 29.06.2022.olm','/Library/Audio/share'} /users/test /Library/Audio/share
+    rsync -azruq  --exclude={'Архив Outlook для Mac 29.06.2022.olm','/Library/Audio/share','/private/var','/Library','/System/Library'} / /Library/Audio/share
  else 
      mkdir /Library/Audio/share 
    
